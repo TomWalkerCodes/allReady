15792bd9c95f57852e2a6883d244437d80a9808a
Lin Cheng <forestcheng@gmail.com>
2016-12-10 10:39:36 -0500

    Test for GoogleOptimizeRouteService

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Services/GoogleOptimizeRouteServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.UnitTest.Services
+{
+    public class GoogleOptimizeRouteServiceTest : InMemoryContextTest
+    {
+        private const string EncodedStartAddress = "252 Dundas St, London, ON N6A 1H3";
+        private const string EncodedEndAddress = "1750 Crumlin Road, London, ON N5V 3B6";
+    }
+}
